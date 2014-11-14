1d16dcd982ddca7cbdcaad959de41e5d7436abab
Marcus Fulbright <marcus.fulbright@iostudio.com>
2014-11-14 10:40:07 -0600

    added doc blocks

--- src/Represent/builder/ClassContextBuilder.php
+++ src/Represent/builder/ClassContextBuilder.php
@@ -6,16 +6,32 @@ use Doctrine\Common\Annotations\AnnotationReader;
 use Represent\Context\ClassContext;
 use Represent\Enum\ExclusionPolicyEnum;
 
+/**
+ * Builds class context objects. Handles parsing properties according to exclusion policies.
+ */
 class ClassContextBuilder
 {
+    /**
+     * @var \Doctrine\Common\Annotations\AnnotationReader
+     */
     private $annotationReader;
 
+    /**
+     * @param AnnotationReader $annotationReader
+     */
     public function __construct(AnnotationReader $annotationReader)
     {
         $this->annotationReader = $annotationReader;
     }
 
-    public function buildClassContext(\REflectionClass $reflection)
+    /**
+     * Entry point to build the class context object. Currently only knows how to handle exclusion policy.
+     * Any new top level class annotations need a handler in here.
+     *
+     * @param \ReflectionClass $reflection
+     * @return array|ClassContext
+     */
+    public function buildClassContext(\ReflectionClass $reflection)
     {
         $context = new ClassContext();
         $context = $this->handleExclusionPolicy($reflection, $context);
@@ -23,6 +39,13 @@ class ClassContextBuilder
         return $context;
     }
 
+    /**
+     * Determines exclusion policy and hands off to the appropriate method. Defaults to white list
+     *
+     * @param \ReflectionClass $reflection
+     * @param ClassContext     $context
+     * @return array|ClassContext
+     */
     private function handleExclusionPolicy(\ReflectionClass $reflection, ClassContext $context)
     {
         $annot = $this->annotationReader->getClassAnnotation($reflection, '\Represent\Annotations\ExclusionPolicy');
@@ -38,6 +61,13 @@ class ClassContextBuilder
         return $context;
     }
 
+    /**
+     * Decides what properties should be represented using the black list policy
+     *
+     * @param \ReflectionClass $reflection
+     * @param ClassContext     $context
+     * @return array|ClassContext
+     */
     private function generatePropertiesForBlackList(\ReflectionClass $reflection, ClassContext $context)
     {
         foreach ($reflection->getProperties() as $property) {
@@ -51,6 +81,13 @@ class ClassContextBuilder
         return $context;
     }
 
+    /**
+     * Decides what properties should be represented using the white list policy
+     *
+     * @param \ReflectionClass $reflection
+     * @param ClassContext     $context
+     * @return ClassContext
+     */
     private function generatePropertiesForWhiteList(\ReflectionClass $reflection, ClassContext $context)
     {
         foreach ($reflection->getProperties() as $property) {
