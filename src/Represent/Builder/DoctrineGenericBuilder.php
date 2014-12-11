81bfb52f2adbc24ec748cb8692bf247fdfcdf7df
Marcus Fulbright <marcus.fulbright@iostudio.com>
2014-12-11 16:09:59 -0600

    update the doctrine workaround

--- src/Represent/Builder/DoctrineGenericBuilder.php
+++ src/Represent/Builder/DoctrineGenericBuilder.php
@@ -3,6 +3,7 @@
 namespace Represent\Builder;
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
 
 class DoctrineGenericBuilder extends GenericRepresentationBuilder
 {
@@ -21,9 +22,40 @@ class DoctrineGenericBuilder extends GenericRepresentationBuilder
     protected function handleObject($object, $view)
     {
         if ($object instanceof \Doctrine\ORM\Proxy\Proxy) {
-            $object = $this->em->find(get_class($object), $object->getPrimaryKey());
+            $object = $this->handleDoctrineProxy($object);
         }
 
-        return parent::handleObject($object, $view);
+        $hash   = spl_object_hash($object);
+        $check  = array_search($hash, $this->visited);
+        $output = new \stdClass();
+
+        if ($check !== false) {
+            $output = new \stdClass();
+            $rel    = '$rel';
+            $output->$rel = $check;
+
+            return $output;
+        }
+        $output->_hash = count($this->visited);
+        $this->visited[] = $hash;
+        $reflection      = new \ReflectionClass($object);
+        $classContext    = $this->classBuilder->buildClassContext($reflection, $hash, $view);
+
+        foreach ($classContext->properties as $property) {
+            $output = $this->handleProperty($property, $object, $output, $classContext);
+        }
+
+        return $output;
+    }
+
+    private function handleDoctrineProxy(\Doctrine\ORM\Proxy\Proxy $proxy)
+    {
+        do {
+            $this->em->detach($proxy);
+            $object = $this->em->find(get_class($proxy), $proxy->getPrimaryKey());
+            $this->em->merge($object);
+        } while ($object instanceof \Doctrine\ORM\Proxy\Proxy);
+
+        return $object;
     }
 }
